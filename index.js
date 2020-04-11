ea96e0e60c8c06ea862cfab18ba60842f020e497
shanik hernandez <shaniik.her93@gmail.com>
2020-04-10 22:18:15 -0400

    mysql connection

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    // username
+    user: "root",
+  
+    // password
+    password: "root17",
+    database: "top_songsDB"
+  });
