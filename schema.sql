6eaa0a7e62b89acfa0b8352506e58ff9014f4291
shanik hernandez <shaniik.her93@gmail.com>
2020-04-11 16:00:05 -0400

    sql table role and department

--- schema.sql
+++ schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY);
+
+CREATE TABLE role (
+    title VARCHAR(30) NOT NULL,
+    salary VARCHAR (20) NOT NULL,
+    department_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY);
+
+CREATE TABLE department(
+    name VARCHAR(50) NOT NULL);
